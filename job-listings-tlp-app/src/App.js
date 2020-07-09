67d238794d92ec8f95d5bcf28cb6f3139e4a80b3
T. Lanette <TLPollard4516@gmail.com>
2020-07-09 18:42:36 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- job-listings-tlp-app/src/App.js
+++ job-listings-tlp-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import FilterBar from  './components/FilterBar';
 
 import data from '../src/data.json';
 import './App.scss';
@@ -14,7 +15,7 @@ function App() {
         <React.Fragment>
             <Header></Header>
             <div className="container">
-
+                
             </div>
             <Footer></Footer>
         </React.Fragment>    
