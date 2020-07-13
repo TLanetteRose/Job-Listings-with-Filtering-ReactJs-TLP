9cbc0fe2e7a3d84329531b970f70e48c8bb57f78
T. Lanette <TLPollard4516@gmail.com>
2020-07-13 09:57:36 -0400

    Starting Again
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- job-listings-tlp-app/src/App.js
+++ job-listings-tlp-app/src/App.js
@@ -1,28 +1,3 @@
-import React from 'react';
-import './App.scss';
-
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { JobsList } from './components/JobsList';
-
-
-function App() {
-
-    return (
-        <React.Fragment>
-            <Header />
-            <JobsList />
-            <Footer />
-        </React.Fragment>
-    )
-}
-
-
-export default App;
-
-
-
-
 
 
 
