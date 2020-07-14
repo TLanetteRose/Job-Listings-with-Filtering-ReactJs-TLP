4704285af873dea46d6fe53670772955f3ca0513
T. Lanette <TLPollard4516@gmail.com>
2020-07-13 21:18:02 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- job-listings-tlp-app/src/App.js
+++ job-listings-tlp-app/src/App.js
@@ -1,3 +1,20 @@
+import React from 'react';
+import './App.scss';
 
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
+function App() {
+    return (
+        <React.Fragment>
+            <Header />
+            <div className='container'>
+            </div>
+            <Footer />
+        </React.Fragment>
 
+    );
+
+}
+
+export default App;
